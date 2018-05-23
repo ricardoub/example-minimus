372a8d092e9f25b97c29d87bcff4ecb178447bb7
Ricardo Bomfim <ricardoub@gmail.com>
2018-05-23 18:38:42 -0300

    weather card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
 //import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AddCardComponent,
     //WeatherCardComponent
   ],
   imports: [
