81e762d6cef6b8880011aeebda1531d80eb91910
Ricardo Bomfim <ricardoub@gmail.com>
2018-05-24 18:55:57 -0300

    routing, login e=and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './ui/details/details.component';
+import { AddCardComponent} from './ui/add-card/add-card.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'details/:city', component: DetailsComponent },
+  { path: 'add', component: AddCardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AddCardComponent } from './ui/add-card/add-card.component';
 import { DetailsComponent } from './ui/details/details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 //import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 
 @NgModule({
@@ -14,6 +16,8 @@ import { DetailsComponent } from './ui/details/details.component';
     HomeComponent,
     AddCardComponent,
     DetailsComponent,
+    LoginComponent,
+    SignupComponent,
     //WeatherCardComponent
   ],
   imports: [
